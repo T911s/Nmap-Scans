6294d69c47be268ec0fcbb064aa8b426141a60cf
T911s <timothyborninkhof@gmail.com>
2017-07-04 22:24:43 +1000

    Enumeration

--- Enumeration Script.sh
+++ Enumeration Script.sh
@@ -28,29 +28,31 @@ for ip in $(cat /root/exam6/nmap_scans/iplist.txt); do
   mkdir -p /root/exam6/nmap_scans/$ip/
 
   printf "\n"
-  printf "${RED}[+]${RESET} ${BLUE}Fast nmap scan for $ip...\n"
+  printf "${RED}[+]${RESET} ${BLUE}Fast nmap scan for $ip...${RESET}\n"
   printf "\n"
   nmap -v -sV -Pn -T4 -oX /root/exam6/nmap_scans/$ip/fast-scan.xml $ip && xsltproc /root/exam6/nmap_scans/$ip/fast-scan.xml -o /root/exam6/nmap_scans/$ip/fast-scan-report.html
   firefox /root/exam6/nmap_scans/$ip/fast-scan-report.html
 
   printf "\n"
-  printf "${RED}[+]${RESET} ${BLUE}UDP nmap scan for $ip...{RESET}\n"
+  printf "${RED}[+]${RESET} ${BLUE}UDP nmap scan for $ip...${RESET}\n"
   printf "\n"
-  nmap -sU -vv -Pn --stats-every 3m --max-retries 2 -oX /root/exam6/nmap_scans/$ip/udp-scan.xml $ip && xsltproc /root/exam6/nmap_scans/$ip/udp-scan.xml -o /root/exam6/nmap_scans/$$
+  nmap -sU -vv -Pn --stats-every 3m --max-retries 2 -oX /root/exam6/nmap_scans/$ip/udp-scan.xml $ip && xsltproc /root/exam6/nmap_scans/$ip/udp-scan.xml \
+  -o /root/exam6/nmap_scans/$ip/udp-scan-report.html
   firefox /root/exam6/nmap_scans/$ip/udp-scan-report.html
 
+  # Might run enume4linux, onesixtyone, gobuster, and nikto as nohup process for the background to speed this step up
   printf "\n"
-  printf "${RED}[+]${RESET} ${BLUE}Enum4linux scan for $ip...{RESET}\n"
+  printf "${RED}[+]${RESET} ${BLUE}Enum4linux scan for $ip...${RESET}\n"
   printf "\n"
   enum4linux -a $ip >> /root/exam6/nmap_scans/$ip/enum4linux_results.txt
 
   printf "\n"
-  printf "${RED}[+]${RESET} ${BLUE}onesixtyone scan for $ip...{RESET}\n"
+  printf "${RED}[+]${RESET} ${BLUE}onesixtyone scan for $ip...${RESET}\n"
   printf "\n"
   onesixtyone $ip -o /root/exam6/nmap_scans/$ip/onesixtyone_results.txt
 
   printf "\n"
-  printf "${RED}[+]${RESET} ${BLUE}Gobuster scripts $ip...{RESET}\n"
+  printf "${RED}[+]${RESET} ${BLUE}Gobuster scripts $ip...${RESET}\n"
   printf "\n"
   printf "Starting gobuster script with common.txt wordlist"
   gobuster -u http://$ip -w /root/wordlists/common.txt -s '200,204,301,302,307,403,500' -e >> /root/exam6/nmap_scans/$ip/gobuster-common_$ip.txt
@@ -60,13 +62,13 @@ for ip in $(cat /root/exam6/nmap_scans/iplist.txt); do
 # gobuster -u http://$ip -w /root/wordlists/big.txt -s "200,204,301,302,307,403,500' -e  >> /root/exam6/nmap_scans/$ip/gobuster-big_$ip.txt
   
   printf "\n"
-  printf "${RED}[+]${RESET} ${BLUE}Nikto for $ip...{RESET}\n"
+  printf "${RED}[+]${RESET} ${BLUE}Nikto for $ip...${RESET}\n"
   printf "\n"
   nikto -h $ip -F html -output /root/exam6/nmap_scans/$ip/nikto_$ip.html
   firefox /root/exam6/nmap_scans/$ip/nikto_$ip.html
 
   printf "\n"
   printf "++++++++++++++++++++"
-  printf "${GREEN}starting next host!{RESET}"
+  printf "${GREEN}starting next host!${RESET}"
   printf "++++++++++++++++++++"
 done
