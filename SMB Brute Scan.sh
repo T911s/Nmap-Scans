2d0722bcff267778c7cac6b9265900293764415d
T911s <timothyborninkhof@gmail.com>
2017-07-11 21:22:19 +1000

    Enumeration

--- SMB Brute Scan.sh
+++ SMB Brute Scan.sh
@@ -1,12 +1,14 @@
   #!/bin/bash
 
   # go to seclists to get the username and passwords ;)
+  # wordlists stored at /root/wordlists/
+
   printf "${RED}[+]${RESET} ${BLUE} Checking smb hashes with smb-brute NSE script over $ip...${RESET}\n"
   printf "Note: This may close the port\n"
-  printf "Enter IP: \n"
+  printf "Enter IP: "
   read ip
 
-    nmap -sS -vv -p 445 --script=smb-brute --script-args=userdb=usernames.txt,passdb=passwords.txt $ip \
+    nmap -vv -p 445 --script=smb-brute --script-args=userdb=/root/wordlists/default_usernames.txt,passdb=/root/wordlists/default_passwords.txt \
       -oX /root/exam/nmap_scans/$ip/smb_nse_brute.xml $ip && xsltproc /root/exam/nmap_scans/$ip/smb_nse_brute.xml \
       -o /root/exam/nmap_scans/$ip/smb_nse_brute_report.html
   firefox /root/exam/nmap_scans/$ip/smb_nse_brute_report.html
