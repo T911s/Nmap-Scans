67fadefaa4247f007f06f9ad7a48541ecb02d61e
T911s <timothyborninkhof@gmail.com>
2017-07-11 22:13:00 +1000

    Enumeration

--- SMB Brute Scan.sh
+++ SMB Brute Scan.sh
@@ -11,8 +11,13 @@ read ip
 nmap -vv -p 445 --script=smb-brute --script-args=userdb=/root/wordlists/default_usernames.txt,passdb=/root/wordlists/default_passwords.txt \
   -oX /root/exam/nmap_scans/$ip/smb_nse_brute.xml $ip && xsltproc /root/exam/nmap_scans/$ip/smb_nse_brute.xml \
   -o /root/exam/nmap_scans/$ip/smb_nse_brute_report.html
+  
+firefox
+sleep 2;
 firefox /root/exam/nmap_scans/$ip/smb_nse_brute_report.html
 sleep 1;
+
+exit
 
 #if its not found, try the other password files: 10k-most-common-passwords,500-worst-passwords,default-passwords
 
---
+++ SMB Pwdump Scan.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# requires the smb-pwdump.nse to be downloaded from:
+# https://svn.nmap.org/nmap-exp/dev/nmap/scripts/smb-pwdump.nse?p=25000
+# Note: not currently working, theres an issue with an smb return
+
+printf "[+] Checking smb hashes with smb-pwdump NSE script over $ip...\n"
+printf "Note: This may close the port\n"
+printf "Enter IP: "
+read ip
+
+  nmap -vv -p 135,139,445 --script=smb-pwdump.nse --script-args=smbuser=msfadmin,smbpass=msfadmin \
+  -oX /root/exam/nmap_scans/$ip/smb_nse_pwdump.xml $ip && xsltproc /root/exam/nmap_scans/$ip/smb_nse_pwdump.xml \
+  -o /root/exam/nmap_scans/$ip/smb_nse_pwdump_report.html
+
+  firefox
+  sleep 2;
+  firefox /root/exam/nmap_scans/$ip/smb_nse_pwdump_report.html
+  sleep 5;
+  printf "\n"
+
+exit
