21eea92e4962ae5e110332a1be7c32fb09037a18
T911s <timothyborninkhof@gmail.com>
2017-07-26 18:17:23 +1000

    Enumeration

--- Information_Gathering.sh
+++ Information_Gathering.sh
@@ -13,7 +13,7 @@ function enumeration_scan {
   echo ""
   echo "               w----------------------------------------------------------------w"
   echo "               |                                                                |"
-  echo "               |                   Nmap Enumeration Scan                        |"
+  echo "               |                Nmap Infomation Gathering Scan                  |"
   echo "               |                                                         -t911  |"
   echo "               w----------------------------------------------------------------w"
   echo ""
@@ -27,120 +27,118 @@ function next_host {
   printf "\n"
 }
 
-  enumeration_scan  
+  enumeration_scan
 
 # do a nmap tcp all ports scan and run searchsploit on the results
 
+function tcp_all_ports {
   echo ""
-  echo "                ***********************************************************************"
-  echo "                |                                                                     |"
-  echo "                |               Now starting a TCP all ports/UDP scan!                |"  
-  echo "                |                                                                     |"
-  echo "                ***********************************************************************"
+  echo "            ***********************************************************************"
+  echo "            |                                                                     |"
+  echo "            |               Now starting a TCP all ports/UDP scan!                |"
+  echo "            |                                                                     |"
+  echo "            ***********************************************************************"
   echo ""
+}
 
-for ip in $(cat /root/exam/nmap_scans/iplist.txt); do
-mkdir -p /root/exam/nmap_scans/$ip/
+  tcp_all_ports
+
+# Identify all tcp ports running on the ip addresses in the list.
+
+for ip in $(cat /root/exam/oscp_tools/iplist.txt); do
+mkdir -p /root/exam/hosts/$ip/nmap_scans/
 
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE}TCP all ports nmap scan for $ip...${RESET}\n"
   printf "\n"
-  nmap -vv -sS -Pn -T4 -p- -oX /root/exam/nmap_scans/$ip/allports-scan.xml $ip && xsltproc /root/exam/nmap_scans/$ip/allports-scan.xml \
-  -o /root/exam/nmap_scans/$ip/allports-scan-report.html
-  sleep 5;
+  nmap -vv -sS -Pn -T4 -p- -oX /root/exam/hosts/$ip/nmap_scans/allports-scan.xml $ip && xsltproc /root/exam/hosts/$ip/nmap_scans/allports-scan.xml \
+  -o /root/exam/hosts/$ip/nmap_scans/allports-scan-report.html
+  sleep 2;
 
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE}UDP nmap scan for $ip...${RESET}\n"
   printf "\n"
-  nmap -sU -vv -Pn --stats-every 3m --max-retries 2 -oX /root/exam/nmap_scans/$ip/udp-scan.xml $ip && xsltproc /root/exam/nmap_scans/$ip/udp-scan.xml \
-  -o /root/exam/nmap_scans/$ip/udp-scan-report.html
-  
-  sleep 5;
+  nmap -sU -vv -Pn --stats-every 3m --max-retries 1 -oX /root/exam/hosts/$ip/nmap_scans/udp-scan.xml $ip && xsltproc /root/exam/hosts/$ip/nmap_scans/udp-scan.xml \
+  -o /root/exam/hosts/$ip/nmap_scans/udp-scan-report.html
+
+  sleep 2;
   /usr/bin/firefox &
-  firefox /root/exam/nmap_scans/$ip/allports-scan-report.html
-  firefox /root/exam/nmap_scans/$ip/udp-scan-report.html
-  
+  firefox /root/exam/hosts/$ip/nmap_scans/allports-scan-report.html
+  firefox /root/exam/hosts/$ip/nmap_scans/udp-scan-report.html
+
   next_host
 done
-    
+
 # do a detailed nmap scan over all tcp ports
 
   echo ""
   echo "                 ************************************************************************"
   echo "                 |                                                                       |"
-  echo "                 |               Now starting detailed all ports TCP scan!               |"  
+  echo "                 |               Now starting detailed all ports TCP scan!               |"
   echo "                 |                       This may take a while...                        |"
   echo "                 |                                                                       |"
   echo "                 ************************************************************************"
   echo ""
 
-for ip in $(cat /root/exam/nmap_scans/iplist.txt); do
+for ip in $(cat /root/exam/oscp_tools/iplist.txt); do
 
-  printf "\n"
+printf "\n"
   printf "${RED}[+]${RESET} ${BLUE}Detailed TCP nmap scan for $ip...${RESET}\n"
   printf "\n"
-  nmap -vv -sV -sC -Pn --reason --version-all -T4 -p- -A -oX /root/exam/nmap_scans/$ip/detailed-scan.xml $ip && xsltproc /root/exam/nmap_scans/$ip/detailed-scan.xml \
-  -o /root/exam/nmap_scans/$ip/detailed-scan-report.html
+  nmap -vv -sV -sC -Pn --reason --version-all -T4 -p- -A -oX /root/exam/hosts/$ip/nmap_scans/detailed-scan.xml $ip && xsltproc /root/exam/hosts/$ip/nmap_scans/detailed-scan.xml \
+  -o /root/exam/hosts/$ip/nmap_scans/detailed-scan-report.html
   printf "\n"
   printf "Now running searchsploit over results\n"
   printf "Please advise this is not 100 percent and manual testings are preferred, due to nmap output\n"
   printf "\n"
   # printf "View results with #cat searchsploit-results.xml\n"
   sleep 2;
-  searchsploit -v --nmap /root/exam/nmap_scans/$ip/detailed-scan.xml >> /root/exam/nmap_scans/$ip/detailed_searchsploit-results.xml
+  searchsploit -v --nmap /root/exam/hosts/$ip/nmap_scans/detailed-scan.xml >> /root/exam/hosts/$ip/nmap_scans/detailed_searchsploit-results.xml
   printf "\n"
-  firefox /root/exam/nmap_scans/$ip/detailed-scan-report.html
+  firefox /root/exam/hosts/$ip/nmap_scans/detailed-scan-report.html
   sleep 5;
-  
+
   next_host
 done
 
   echo ""
   echo "                *****************************************************************"
   echo "                |                                                               |"
-  echo "                |                Now starting the Nmap NSE scan!                |"  
+  echo "                |                Now starting the Nmap NSE scan!                |"
   echo "                |                                                               |"
   echo "                *****************************************************************"
   echo ""
 
 # Run a NSE Scan for all IP addresses in iplist.txt and output to firefox
 
-for ip in $(cat /root/exam/nmap_scans/iplist.txt); do
+for ip in $(cat /root/exam/oscp_tools/iplist.txt); do
 
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE} Nmap FTP NSE scan over port 21 for $ip...${RESET}\n"
   printf "\n"
   nmap -sS -vv -Pn -p 21 --script=ftp-anon,ftp-bounce,ftp-libopie,ftp-proftpd-backdoor,ftp-vsftpd-backdoor,ftp-vuln-cve2010-4221 \
-  -oX /root/exam/nmap_scans/$ip/ftp_port21.xml $ip && xsltproc /root/exam/nmap_scans/$ip/ftp_port21.xml \
-  -o /root/exam/nmap_scans/$ip/ftp_port21_report_$ip.html
-  sleep 5;
+  -oX /root/exam/hosts/$ip/nmap_scans/ftp_port21.xml $ip && xsltproc /root/exam/hosts/$ip/nmap_scans/ftp_port21.xml \
+  -o /root/exam/hosts/$ip/nmap_scans/ftp_port21_report_$ip.html
+  sleep 2;
 
 # I havent added nmap nse script for port 22
 # This will usually just be for brute forcing the host
 
-  printf "\n"
+printf "\n"
   printf "${RED}[+]${RESET} ${BLUE} Nmap SMTP NSE scan over port 25 for $ip...${RESET}\n"
   printf "\n"
   nmap -sS -vv -p 25 --script=smtp-commands,smtp-enum-users,smtp-open-relay,smtp-vuln-cve2010-4344,smtp-vuln-cve2011-1720,smtp-vuln-cve2011-1764 \
-  -oX /root/exam/nmap_scans/$ip/smtp_nse.xml $ip && xsltproc /root/exam/nmap_scans/$ip/smtp_nse.xml \
-  -o /root/exam/nmap_scans/$ip/smtp_nse_report.html
-  sleep 5;
-
-  printf "\n"
-  printf "${RED}[+]${RESET} ${BLUE} Nmap DNS NSE scan over port 53 for $ip..${RESET}\n"
-  printf "\n"
-  nmap -sV -vv -sC -p 53 --script=broadcast-dns-service-discovery \
-  -oX /root/exam/nmap_scans/$ip/dns_nse.xml $ip && xsltproc /root/exam/nmap_scans/$ip/dns_nse.xml \
-  -o /root/exam/nmap_scans/$ip/dns_nse_report_$ip.html
-  sleep 5;  
+  -oX /root/exam/hosts/$ip/nmap_scans/smtp_nse.xml $ip && xsltproc /root/exam/hosts/$ip/nmap_scans/smtp_nse.xml \
+  -o /root/exam/hosts/$ip/nmap_scans/smtp_nse_report.html
+  sleep 2;2
 
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE} Nmap HTTP NSE scan over port 80 for $ip...${RESET}\n"
   printf "\n"
   nmap -sS -vv -Pn -p 80,8080,8000 --script=http-auth-finder,http-comments-displayer,http-config-backup,http-method-tamper,http-passwd,http-default-accounts,http-robots.txt,http-enum,http-exif-spider,http-fileupload-exploiter,http-php-version,http-sql-injection,http-userdir-enum \
-  -oX /root/exam/nmap_scans/$ip/http_port80.xml $ip && xsltproc /root/exam/nmap_scans/$ip/http_port80.xml \
-  -o /root/exam/nmap_scans/$ip/http_port80_report.html
-  sleep 5;
+  -oX /root/exam/hosts/$ip/nmap_scans/http_port80.xml $ip && xsltproc /root/exam/hosts/$ip/nmap_scans/http_port80.xml \
+  -o /root/exam/hosts/$ip/nmap_scans/http_port80_report.html
+  sleep 2;
 
 # not scanning for pop3
 # not running pop3-brute in this scan
@@ -149,68 +147,66 @@ for ip in $(cat /root/exam/nmap_scans/iplist.txt); do
   printf "${RED}[+]${RESET} ${BLUE} Nmap NFS NSE scan over port 111 for $ip...${RESET}\n"
   printf "\n"
   nmap -sS -vv -Pn -p 111 --script=nfs-ls,nfs-showmount,nfs-statfs \
-  -oX /root/exam/nmap_scans/$ip/nfs_port111.xml $ip && xsltproc /root/exam/nmap_scans/$ip/nfs_port111.xml \
-  -o /root/exam/nmap_scans/$ip/nfs_port111_report.html
-  sleep 5;
+  -oX /root/exam/hosts/$ip/nmap_scans/nfs_port111.xml $ip && xsltproc /root/exam/hosts/$ip/nmap_scans/nfs_port111.xml \
+  -o /root/exam/hosts/$ip/nmap_scans/nfs_port111_report.html
+  sleep 2;
 
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE} Nmap SMB NSE scan over port 139 and 445 for $ip...${RESET}\n"
   printf "\n"
   nmap -sS -Pn -vv -p 139,445 --script=smb-enum-domains,smb-os-discovery,smb-enum-shares,smb-enum-users,smb-enum-sessions,smb-enum-groups,smb-enum-processes,smb-server-stats,smb-system-info,smbv2-enabled \
-  -oX /root/exam/nmap_scans/$ip/smb_nse.xml $ip && xsltproc /root/exam/nmap_scans/$ip/smb_nse.xml \
-  -o /root/exam/nmap_scans/$ip/smb_nse_report.html
-  sleep 5;
+  -oX /root/exam/hosts/$ip/nmap_scans/smb_nse.xml $ip && xsltproc /root/exam/hosts/$ip/nmap_scans/smb_nse.xml \
+  -o /root/exam/hosts/$ip/nmap_scans/smb_nse_report.html
+  sleep 2;
 
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE} Nmap SMB_Vulns NSE scan over port 139 and 445 for $ip...${RESET}\n"
   printf "\n"
   nmap -sS -vv -p 139,445 --script-args=unsafe=1 --script=smb-vuln-conficker,smb-vuln-cve2009-3103,smb-vuln-ms06-025,smb-vuln-ms07-029,smb-vuln-ms08-067,smb-vuln-ms10-054,smb-vuln-ms10-061,smb-vuln-regsvc-dos \
-  -oX /root/exam/nmap_scans/$ip/smb_nse_vuln.xml $ip && xsltproc /root/exam/nmap_scans/$ip/smb_nse_vuln.xml \
-  -o /root/exam/nmap_scans/$ip/smb_nse_vuln_report.html
-  sleep 5;
+  -oX /root/exam/hosts/$ip/nmap_scans/smb_nse_vuln.xml $ip && xsltproc /root/exam/hosts/$ip/nmap_scans/smb_nse_vuln.xml \
+  -o /root/exam/hosts/$ip/nmap_scans/smb_nse_vuln_report.html
+  sleep 2;
 
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE} Nmap SNMP NSE scan over port 161 for $ip...${RESET}\n"
   printf "\n"
   nmap -sS -vv -p 161 --script=snmp-info,snmp-netstat,snmp-processes,snmp-sysdescr,snmp-win32-services,snmp-win32-shares,snmp-win32-software,snmp-win32-users \
-  -oX /root/exam/nmap_scans/$ip/snmp_nse.xml $ip && xsltproc /root/exam/nmap_scans/$ip/snmp_nse.xml \
-  -o /root/exam/nmap_scans/$ip/snmp_nse_report.html
-  sleep 5;
+  -oX /root/exam/hosts/$ip/nmap_scans/snmp_nse.xml $ip && xsltproc /root/exam/hosts/$ip/nmap_scans/snmp_nse.xml \
+  -o /root/exam/hosts/$ip/nmap_scans/snmp_nse_report.html
+  sleep 2;
 
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE} Nmap HTTPS NSE scan over port 443 for $ip...${RESET}\n"
   printf "\n"
   nmap -sS -vv -p 443 --script-args vulns.showall --script=ssl-heartbleed,ssl-poodle,ssl-dh-params \
-  -oX /root/exam/nmap_scans/$ip/https_nse.xml $ip && xsltproc /root/exam/nmap_scans/$ip/https_nse.xml \
-  -o /root/exam/nmap_scans/$ip/https_nse_report.html
-  sleep 5;  
+  -oX /root/exam/hosts/$ip/nmap_scans/https_nse.xml $ip && xsltproc /root/exam/hosts/$ip/nmap_scans/https_nse.xml \
+  -o /root/exam/hosts/$ip/nmap_scans/https_nse_report.html
+  sleep 2;
 
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE} Nmap MySQL NSE scan over port 3306 for $ip...${RESET}\n"
   printf "\n"
   nmap -sS -vv -p 445,1433,3306 --script=ms-sql-info,mysql-audit,mysql-databases,mysql-dump-hashes,mysql-empty-password,mysql-enum,mysql-info,mysql-query,mysql-users,mysql-variables,mysql-vuln-cve2012-2122 \
-  -oX /root/exam/nmap_scans/$ip/mysql_nse.xml $ip && xsltproc /root/exam/nmap_scans/$ip/mysql_nse.xml \
-  -o /root/exam/nmap_scans/$ip/mysql_nse_report.html
-  sleep 5;
+  -oX /root/exam/hosts/$ip/nmap_scans/mysql_nse.xml $ip && xsltproc /root/exam/hosts/$ip/nmap_scans/mysql_nse.xml \
+  -o /root/exam/hosts/$ip/nmap_scans/mysql_nse_report.html
+  sleep 2;
 
-    printf "Now to output all NSE scans for $ip to firefox!\n"
-    firefox /root/exam/nmap_scans/$ip/ftp_port21_report_$ip.html
-    firefox /root/exam/nmap_scans/$ip/dns_nse_report_$ip.html
-    firefox /root/exam/nmap_scans/$ip/http_port80_report.html
+printf "Now to output all NSE scans for $ip to firefox!\n"
+    firefox /root/exam/hosts/$ip/nmap_scans/ftp_port21_report_$ip.html
+    firefox /root/exam/hosts/$ip/nmap_scans/http_port80_report.html
     sleep 2;
-    firefox /root/exam/nmap_scans/$ip/nfs_port111_report.html
-    firefox /root/exam/nmap_scans/$ip/smb_nse_report.html
-    firefox /root/exam/nmap_scans/$ip/smb_nse_vuln_report.html
+    firefox /root/exam/hosts/$ip/nmap_scans/nfs_port111_report.html
+    firefox /root/exam/hosts/$ip/nmap_scans/smb_nse_report.html
+    firefox /root/exam/hosts/$ip/nmap_scans/smb_nse_vuln_report.html
     sleep 2;
-    firefox /root/exam/nmap_scans/$ip/snmp_nse_report.html
-    firefox /root/exam/nmap_scans/$ip/https_nse_report.html
-    firefox /root/exam/nmap_scans/$ip/mysql_nse_report.html
+    firefox /root/exam/hosts/$ip/nmap_scans/snmp_nse_report.html
+    firefox /root/exam/hosts/$ip/nmap_scans/https_nse_report.html
+    firefox /root/exam/hosts/$ip/nmap_scans/mysql_nse_report.html
     sleep 2;
 done
 
 printf "${RED}[+]${RESET} Scans completed\n"
 printf "${RED}[+]${RESET} Results saved to /root/exam/nmap_scans/'IP_ADDRESS'\n"
 printf "${RED}[+]${RESET} For more port information, follow: 0daySecurity Enumeration\n"
-printf "${RED}[+]${RESET} Now starting Burp Suite for Active Spidering/Web Applications\n"
-burpsuite
+
 exit
--- Multi Tool Scan.sh
+++ Multi Tool Scan.sh
@@ -2,6 +2,10 @@
 
 # requires list of addresses in /root/exam/nmap_scans/iplist.txt
 
+# in terms of seperating infomation gathering and enumeration, it could be
+# time reducing if i had to seperate folders /root/exam/$ip/infomation_gather//
+# and /root/exam/$ip/enumeration/
+
   # Colours
   ESC="\e["
   RESET=$ESC"39m"
@@ -30,77 +34,102 @@ function next_host {
 
 enumeration_scan
 
-for ip in $(cat /root/exam/nmap_scans/iplist2.txt); do
-  mkdir -p /root/exam/nmap_scans/$ip/
+for ip in $(cat /root/exam/oscp_tools/iplist2.txt); do
+  mkdir -p /root/exam/hosts/$ip/enumeration/
 
   printf "\n"
-  printf "${RED}[+]${RESET} ${BLUE}onesixtyone scan for $ip...${RESET}\n"
-  onesixtyone -c /root/wordlists/dict.txt $ip \
-  >> /root/exam/nmap_scans/$ip/onesixtyone_results.txt
+  printf "${RED}[+]${RESET} ${BLUE}Fingerprinting NBT version for $ip...${RESET}\n"
+  nbtscan -v $ip \
+  >> /root/exam/hosts/$ip/enumeration/nbtscan_results.txt
+  cat /root/exam/hosts/$ip/enumeration/nbtscan_results.txt
   printf "Completed!\n"
-  sleep 5;
+  sleep 3;
 
   printf "\n"
-  printf "${RED}[+]${RESET} ${BLUE}Samrdump scan for $ip without credentials..${RESET}\n"
-  samrdump $ip \
-  >> /root/exam/nmap_scans/$ip/samrdump_nocreds_results.txt
+  printf "${RED}[+]${RESET} ${BLUE}Fingerprinting SMB version for $ip...${RESET}\n"
+  smbclient -L //$ip \
+  >> /root/exam/hosts/$ip/enumeration/smbclient_results.txt
+  cat /root/exam/hosts/$ip/enumeration/smbclient_results.txt
   printf "Completed!\n"
-  sleep 5;
+  printf "Make sure you test for null sessions ""...\n"
+  sleep 3;
 
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE}Enum4linux scan for $ip...${RESET}\n"
   printf "${RED}RID Cycling will not be run${RESET}\n"
   enum4linux -v -U -S -G -M -P -o -n $ip \
-  >> /root/exam/nmap_scans/$ip/enum4linux_results.txt
+  >> /root/exam/hosts/$ip/enumeration/enum4linux_results.txt
+  cat /root/exam/hosts/$ip/enumeration/enum4linux_results.txt
   printf "Completed!\n"
-  sleep 5;
+  sleep 3;
+
+  printf "\n"
+  printf "${RED}[+]${RESET} ${BLUE}Samrdump scan for $ip without credentials..${RESET}\n"
+  samrdump.py $ip \
+  >> /root/exam/hosts/$ip/enumeration/samrdump_nocreds_results.txt
+  cat /root/exam/hosts/$ip/enumeration/samrdump_nocreds_results.txt
+  printf "Completed!\n"
+  sleep 3;
 
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE}snmp-check scan for $ip over UDP 161${RESET}\n"
   perl /root/tools/snmp-check/snmp-check.pl -t $ip -c public \
-  >> /root/exam/nmap_scans/$ip/snmp-check_results.txt
+  >> /root/exam/hosts/$ip/enumeration/snmp-check_results.txt
+  cat /root/exam/hosts/$ip/enumeration/snmp-check_results.txt
   printf "Completed!\n"
-  sleep 5;
+  sleep 3;
+
+  printf "\n"
+  printf "${RED}[+]${RESET} ${BLUE}onesixtyone scan for $ip...${RESET}\n"
+  onesixtyone -c /root/wordlists/dict.txt $ip \
+  >> /root/exam/hosts/$ip/enumeration/onesixtyone_results.txt
+  cat /root/exam/hosts/$ip/enumeration/onesixtyone_results.txt
+  printf "Completed!\n"
+  sleep 3;
 
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE}smtp-user-enum scan for $ip...${RESET}\n"
   smtp-user-enum -M VRFY -U /root/wordlists/names.txt -t $ip \
-  >> /root/exam/nmap_scans/$ip/smtp-users_results.txt
+  >> /root/exam/hosts/$ip/enumeration/smtp-users_results.txt
+  cat /root/exam/hosts/$ip/enumeration/smtp-users_results.txt
   printf "Completed!\n"
-  sleep 5;  
+  sleep 3;  
   
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE}Gobuster scripts $ip... on port 80..${RESET}\n"
   printf "Starting gobuster script with common.txt wordlist against http://$ip/\n"
   gobuster -u http://$ip -w /root/wordlists/common.txt -s '200,204,301,302,307,403,500' -e \
-  >> /root/exam/nmap_scans/$ip/gobuster-common_wordlist.txt
+  >> /root/exam/hosts/$ip/enumeration/gobuster-common_wordlist.txt
+  cat /root/exam/hosts/$ip/enumeration/gobuster-common_wordlist.txt
   printf "Completed!\n"
   printf "Remember to check any subdirectories ;)\n"
-  sleep 5;
+  sleep 3;
 
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE}Nikto for $ip... on port 80..${RESET}\n"
-  nikto -c all v -h http://$ip -Format html -output /root/exam/nmap_scans/$ip/nikto_scan.html
-  firefox /root/exam/nmap_scans/$ip/nikto_scan.html
+  nikto -c all v -h http://$ip -Format xml -output /root/exam/hosts/$ip/enumeration/nikto_scan.xml
+  cat /root/exam/hosts/$ip/enumeration/nikto_scan.xml
   printf "Completed!\n"
-  sleep 5;
+  sleep 3;
 
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE}Ident for $ip... on port 113..${RESET}\n"
   ident-user-enum $ip 22 53 111 113 512 513 514 515 \
-  >> /root/exam/nmap_scans/$ip/ident_scan.html
+  >> /root/exam/hosts/$ip/enumeration/ident_scan.txt
+  cat /root/exam/hosts/$ip/enumeration/ident_scan.txt
   printf "Completed!\n"
-  sleep 5;
+  sleep 3;
 
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE}Reverse dnsrecon lookup for $ip... on port 53..${RESET}\n"
   dnsrecon.py -r $ip  \
-  >> /root/exam/nmap_scans/$ip/reversedns_scan.html
+  >> /root/exam/hosts/$ip/enumeration/reversedns_scan.txt
+  cat /root/exam/hosts/$ip/enumeration/reversedns_scan.txt
   printf "Completed!\n"
-  sleep 5;
+  sleep 3;
 
   next_host
 
-printf "All results located at /root/exam/IP_Address/\n"
+printf "All results located at /root/exam/hosts/IP_Address/\n"
 done
 exit
