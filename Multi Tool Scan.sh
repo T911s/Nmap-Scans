d5a95b6b4c4b96b7a65ed4b2d6445aa09b310027
T911s <timothyborninkhof@gmail.com>
2017-08-05 13:29:30 +1000

    Enumeration

--- Multi Tool Scan.sh
+++ Multi Tool Scan.sh
@@ -40,7 +40,7 @@ for ip in $(cat /root/exam/oscp_tools/iplist2.txt); do
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE}Fingerprinting NBT version for $ip...${RESET}\n"
   nbtscan -v $ip \
-  >> /root/exam/hosts/$ip/enumeration/nbtscan_results.txt
+  > /root/exam/hosts/$ip/enumeration/nbtscan_results.txt
   cat /root/exam/hosts/$ip/enumeration/nbtscan_results.txt
   printf "Completed!\n"
   sleep 3;
@@ -48,7 +48,7 @@ for ip in $(cat /root/exam/oscp_tools/iplist2.txt); do
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE}Fingerprinting SMB version for $ip...${RESET}\n"
   smbclient -L //$ip \
-  >> /root/exam/hosts/$ip/enumeration/smbclient_results.txt
+  > /root/exam/hosts/$ip/enumeration/smbclient_results.txt
   cat /root/exam/hosts/$ip/enumeration/smbclient_results.txt
   printf "Completed!\n"
   printf "Make sure you test for null sessions ""...\n"
@@ -58,7 +58,7 @@ for ip in $(cat /root/exam/oscp_tools/iplist2.txt); do
   printf "${RED}[+]${RESET} ${BLUE}Enum4linux scan for $ip...${RESET}\n"
   printf "${RED}RID Cycling will not be run${RESET}\n"
   enum4linux -v -U -S -G -M -P -o -n $ip \
-  >> /root/exam/hosts/$ip/enumeration/enum4linux_results.txt
+  > /root/exam/hosts/$ip/enumeration/enum4linux_results.txt
   cat /root/exam/hosts/$ip/enumeration/enum4linux_results.txt
   printf "Completed!\n"
   sleep 3;
@@ -66,7 +66,7 @@ for ip in $(cat /root/exam/oscp_tools/iplist2.txt); do
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE}Samrdump scan for $ip without credentials..${RESET}\n"
   samrdump.py $ip \
-  >> /root/exam/hosts/$ip/enumeration/samrdump_nocreds_results.txt
+  > /root/exam/hosts/$ip/enumeration/samrdump_nocreds_results.txt
   cat /root/exam/hosts/$ip/enumeration/samrdump_nocreds_results.txt
   printf "Completed!\n"
   sleep 3;
@@ -74,7 +74,7 @@ for ip in $(cat /root/exam/oscp_tools/iplist2.txt); do
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE}snmp-check scan for $ip over UDP 161${RESET}\n"
   perl /root/tools/snmp-check/snmp-check.pl -t $ip -c public \
-  >> /root/exam/hosts/$ip/enumeration/snmp-check_results.txt
+  > /root/exam/hosts/$ip/enumeration/snmp-check_results.txt
   cat /root/exam/hosts/$ip/enumeration/snmp-check_results.txt
   printf "Completed!\n"
   sleep 3;
@@ -82,7 +82,7 @@ for ip in $(cat /root/exam/oscp_tools/iplist2.txt); do
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE}onesixtyone scan for $ip...${RESET}\n"
   onesixtyone -c /root/wordlists/dict.txt $ip \
-  >> /root/exam/hosts/$ip/enumeration/onesixtyone_results.txt
+  > /root/exam/hosts/$ip/enumeration/onesixtyone_results.txt
   cat /root/exam/hosts/$ip/enumeration/onesixtyone_results.txt
   printf "Completed!\n"
   sleep 3;
@@ -90,7 +90,7 @@ for ip in $(cat /root/exam/oscp_tools/iplist2.txt); do
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE}smtp-user-enum scan for $ip...${RESET}\n"
   smtp-user-enum -M VRFY -U /root/wordlists/names.txt -t $ip \
-  >> /root/exam/hosts/$ip/enumeration/smtp-users_results.txt
+  > /root/exam/hosts/$ip/enumeration/smtp-users_results.txt
   cat /root/exam/hosts/$ip/enumeration/smtp-users_results.txt
   printf "Completed!\n"
   sleep 3;  
@@ -99,7 +99,7 @@ for ip in $(cat /root/exam/oscp_tools/iplist2.txt); do
   printf "${RED}[+]${RESET} ${BLUE}Gobuster scripts $ip... on port 80..${RESET}\n"
   printf "Starting gobuster script with common.txt wordlist against http://$ip/\n"
   gobuster -u http://$ip -w /root/wordlists/common.txt -s '200,204,301,302,307,403,500' -e \
-  >> /root/exam/hosts/$ip/enumeration/gobuster-common_wordlist.txt
+  > /root/exam/hosts/$ip/enumeration/gobuster-common_wordlist.txt
   cat /root/exam/hosts/$ip/enumeration/gobuster-common_wordlist.txt
   printf "Completed!\n"
   printf "Remember to check any subdirectories ;)\n"
@@ -114,7 +114,7 @@ for ip in $(cat /root/exam/oscp_tools/iplist2.txt); do
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE}Ident for $ip... on port 113..${RESET}\n"
   ident-user-enum $ip 22 53 111 113 512 513 514 515 \
-  >> /root/exam/hosts/$ip/enumeration/ident_scan.txt
+  > /root/exam/hosts/$ip/enumeration/ident_scan.txt
   cat /root/exam/hosts/$ip/enumeration/ident_scan.txt
   printf "Completed!\n"
   sleep 3;
@@ -122,7 +122,7 @@ for ip in $(cat /root/exam/oscp_tools/iplist2.txt); do
   printf "\n"
   printf "${RED}[+]${RESET} ${BLUE}Reverse dnsrecon lookup for $ip... on port 53..${RESET}\n"
   dnsrecon -r $ip  \
-  >> /root/exam/hosts/$ip/enumeration/reversedns_scan.txt
+  > /root/exam/hosts/$ip/enumeration/reversedns_scan.txt
   cat /root/exam/hosts/$ip/enumeration/reversedns_scan.txt
   printf "Completed!\n"
   sleep 3;
